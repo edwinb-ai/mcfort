7ad2d3af7624a35ae615ae7100e749a2e25f6e43
Edwin Bedolla <developeredwin@gmail.com>
2021-12-07 21:15:26 -0600

    :sparkles:
    Simple multithreading features using OpenMP.

--- include/energies.f90
+++ include/energies.f90
@@ -3,6 +3,7 @@ module energies
     use types
     use parameters
     use potentials
+    use omp_lib
     
     implicit none
     
@@ -20,8 +21,11 @@ contains
         
         ener = 0.0_dp
 
-        do i = 1, np - 1
-            do j = i + 1, np
+        !$omp parallel default(shared) private(i,j,uij,xij,yij,zij,rij)
+        !$omp do reduction(+:ener)
+        do i = 1, np
+            do j = 1, np
+                if (i == j) cycle
                 uij = 0.0_dp
 
                 xij = x(j)-x(i)
@@ -36,11 +40,15 @@ contains
                 rij = norm2([xij, yij, zij])
 
                 if (rij < rc) then
-                    call pseudohs(rij, uij)
+                    call lennardjones(rij, uij)
                     ener = ener + uij
                 end if
             end do
         end do
+        !$omp end do
+        !$omp end parallel
+
+        ener = ener / 2.0_dp
     end subroutine energy
 
     ! This subroutine calculates the difference in energy when a particle is displaced
@@ -53,6 +61,9 @@ contains
         real(dp) :: rij, xij, yij, zij, uij
 
         dener = 0.0_dp ! initializing
+
+        !$omp parallel do default(shared) private(i,xij,yij,zij,rij,uij) &
+        !$omp reduction(+:dener)
         do i = 1, np
             if ( i == no ) cycle
 
@@ -68,9 +79,10 @@ contains
             rij = norm2([xij, yij, zij])
 
             if (rij < rc) then
-                call pseudohs(rij, uij)
+                call lennardjones(rij, uij)
                 dener = dener + uij
             end if
         end do
+        !$omp end parallel do
     end subroutine denergy
 end module energies
